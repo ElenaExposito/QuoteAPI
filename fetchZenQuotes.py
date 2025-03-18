abc279b242e1ccd2653b573b768c677eb8234573
Elena Expósito <elexla97@gmail.com>
2025-03-18 11:53:48 +0100

    Add emissions tracker with CodeCarbon

--- fetchZenQuotes.py
+++ fetchZenQuotes.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from codecarbon import EmissionsTracker
 
 # URL de la API de ZenQuotes
 ZEN_QUOTES_API_URL = "https://zenquotes.io/api/random"
@@ -49,15 +50,20 @@ def simulate_requests():
 # Ejecuta la función principal
 if __name__ == "__main__":
     print("Inspirational Quotes (powered by ZenQuotes)")
-    while True:
-        fetch_quote()
-        # Pregunta al usuario si quiere otra cita
-        more = input("¿Quieres otra cita? (s/n): ").strip().lower()
-        if more != 's':
-            break
-    print("¡Gracias por usar el programa!")
+    tracker = EmissionsTracker()
+    tracker.start()
+    try:
+        while True:
+            fetch_quote()
+            # Pregunta al usuario si quiere otra cita
+            more = input("¿Quieres otra cita? (s/n): ").strip().lower()
+            if more != 's':
+                break
+        print("¡Gracias por usar el programa!")
 
-    # Simula el envío de peticiones a la aplicación FastAPI
-    print("Simulando el envío de peticiones a la aplicación FastAPI...")
-    simulate_requests()
-    print("Simulación completada.")
+        # Simula el envío de peticiones a la aplicación FastAPI
+        print("Simulando el envío de peticiones a la aplicación FastAPI...")
+        simulate_requests()
+        print("Simulación completada.")
+    finally:
+        tracker.stop()
